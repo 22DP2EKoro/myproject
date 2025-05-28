a3200147de19df4a15f739a6023b6d26374f9bd9
22DP2EKoro <131154546+22DP2EKoro@users.noreply.github.com>
2025-05-28 15:57:00 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+
+// ❗ Only include vueDevTools if you have it installed or set up
+// import vueDevTools from 'vite-plugin-vue-devtools';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    // vueDevTools(), // Uncomment this only if you're using and have installed it
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:80',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/explore-riga'),
+      }
+    }
+  }
+});
